dbac9e28dade5fdc1f582e8c06f459bb2f8c8b96
Inquiries@somaticsfn.com <Inquiries@somaticsfn.com>
2022-11-14 12:16:39 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,13 @@
+const styles = StyleSheet.create({});
 import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "somatics_37582",
-  url: "https://somatics_37582.botics.co",
-  api: "https://somatics_37582.botics.co/api/v1"
-}
+  "name": "somatics_37582",
+  "url": "https://somatics_37582.botics.co",
+  "api": "https://somatics_37582.botics.co/api/v1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
-  }
-}
+  },
+  "@modules/basiqmodule": {}
+};
---
+++ screens/Untitled1/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.rshrxNVL}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  rshrxNVL: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%",
+    borderRadius: "50%"
+  },
+  UtRguulY: {
+    left: 102.5,
+    top: 199,
+    position: "absolute",
+    width: 152,
+    height: 32,
+    lineHeight: 14,
+    fontSize: 36,
+    borderRadius: 0,
+    fontWeight: "700",
+    fontFamily: "Lato"
+  },
+  csvbchdf: {
+    position: "unset"
+  },
+  igKNyArh: {
+    left: 139,
+    top: 268,
+    position: "absolute",
+    width: 80,
+    height: 80,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 57,
+    borderWidth: 3
+  },
+  zYhOBOMC: {
+    left: 177,
+    top: 534,
+    position: "absolute",
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
+  }
+});
